360de7f890481c2cc844f8dc371aa5fd625bc844
R0SEWT <rody.vilchez00@gmail.com>
2023-09-30 06:51:04 -0500

    Ahora persona cuenta con un ID y lista de seguidos

--- convertPure.py
+++ convertPure.py
@@ -1,37 +1,35 @@
 import csv
 import json
-import time
 import random
 
 
 def generarbinaryList(n):
-    a = n*[0]
-    for e in a:
-        e = random.randint(0, 1)
+    a = n * [0]
+    for i in range(n):
+        a[i] = random.randint(0, 1)
     return a
-
-def procesarClavesRapido(datitos, row, rapido, tiempo=0):
-    if not rapido:
-        print(row)
-        time.sleep(tiempo)
-    datitos.append(row)
-
 
 csv_file = 'Airline Dataset Updated - v2.csv'
 
 datitos = []
+max_seguidos = 15
 
 with open(csv_file, mode='r',errors="ignore") as file:
     csv_reader = csv.DictReader(file)
-    cont = 0
+    ID = 0
     for row in csv_reader:
-        a = {'nombre': row['First Name'], 'apellido': row['Last Name'], 'genero': row['Gender'], 'edad': row['Age']}
+
+        a = {'ID': ID, 'nombre': row['First Name'], 'apellido': row['Last Name'], 'genero': row['Gender'],
+             'edad': row['Age']}
         a['correo'] = a['nombre'] + '_' + a['apellido'] + a['edad'] + '@gmail.com'
         a['contrasena'] = str(random.randint(10 ** 10, 10 ** 12))
-        row['binaryList'] = generarbinaryList(11)
+        a['interesesBL'] = generarbinaryList(11)
+        n_seguidos = random.randint(0, min(ID, max_seguidos)) # limitador para cantidad de seguidos
+        lista_seguidos = random.sample(range(ID), n_seguidos) # escoge un aleatorio entre los registros ya creados
+        a['seguidosL'] = lista_seguidos
         datitos.append(a)
-        cont += 1
-    print('se han generado {} registros'.format(cont))
+        ID += 1
+    print('se han generado {} registros'.format(ID))
 
 json_file = 'persona.json'
 
