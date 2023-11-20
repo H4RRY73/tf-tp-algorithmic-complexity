0476ae4291afa5c57a22fd81c6dd2bc4470b3163
ROSEWT <rody.vilchez00@gmail.com>
2023-11-19 22:07:05 -0500

    Genera 3 o 4 intereses en el 66% de los casos

--- convertPure.py
+++ convertPure.py
@@ -2,17 +2,22 @@ import csv
 import json
 import random
 
+intereses = ["Deportes", "Tecnologia", "Cine", "Musica", "Viajes", "Gastronomia", "Arte", "Ciencia", "Moda", "Literatura","Videojuegos"]
 
-def generarbinaryList(n):
-    a = n * [0]
-    for i in range(n):
-        a[i] = random.randint(0, 1)
-    return a
+
 
 csv_file = 'Airline Dataset Updated - v2.csv'
 
 datitos = []
 max_seguidos = 5
+
+def generar_intereses():
+    n = random.randint(1, len(intereses))
+    if random.randint(0, 2) % 2 == 0:
+        n = random.randint(3, 4)
+    intereses_seleccionados = random.sample(intereses, n)
+
+    return intereses_seleccionados
 
 with open(csv_file, mode='r',errors="ignore") as file:
     csv_reader = csv.DictReader(file)
@@ -23,7 +28,7 @@ with open(csv_file, mode='r',errors="ignore") as file:
              'edad': row['Age']}
         a['correo'] = a['nombre'] + '_' + a['apellido'] + a['edad'] + '@gmail.com'
         a['contrasena'] = str(random.randint(10 ** 10, 10 ** 12))
-        a['interesesBL'] = generarbinaryList(11)
+        a['intereses'] = generar_intereses()
         n_seguidos = random.randint(0, min(ID, max_seguidos)) # limitador para cantidad de seguidos
         lista_seguidos = random.sample(range(ID), n_seguidos) # escoge un aleatorio entre los registros ya creados
         a['seguidosL'] = lista_seguidos
